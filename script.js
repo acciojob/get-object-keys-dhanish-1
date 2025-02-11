5f4c2c33eddd99c8c5f18cf7421ca67a614d65ec
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-11 11:15:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-// Create the student object
 const student = {
-  name: "John",
+  name: "John"
 };
 
 // Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
-
-// Testing the function
-console.log(getKeys(student)); // Output: ["name"]
-
-// Additional test cases
-const student2 = {
-  name: "Alice",
-  age: 22,
-  city: "New York",
-};
-
-console.log(getKeys(student2)); // Output: ["name", "age", "city"]
-
-const student3 = {
-  name: "Bob",
-};
-
-console.log(getKeys(student3)); // Output: ["name"]
